23a8fabe0a0420191819dc10bc053a1d4ae19305
Tyler Shifflett <tylershifflett@Tylers-MacBook-Pro.local>
2022-05-07 16:52:44 -0400

    Add Engineers & Intern

--- index.js
+++ index.js
@@ -72,4 +72,95 @@ const addManager = () => {
 
         }
     ])
-}
+    .then(managerInput => {
+        const { name, id, email, officeNumber } = managerInput;
+        const manager = new Manager (name, id, email, officeNumber);
+
+        teamArray.push(manager);
+        console.log(Manager);
+    })
+};
+    const addEmployee = () => {
+        console.log('Add Employees to the team');
+
+        return inquirer.prompt ([
+            {
+                type: 'list',
+                name: 'role',
+                message: "Please choose your employee's role",
+                choices: ['Engineer','Intern']
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: "What's the name of the employee?",
+                validate: nameInput => {
+                    if (nameInput){
+                    return true;
+                } else {
+                    console.log ("Please enter an employee's name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter the employee's ID.",
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log ("Please enter the employee's ID!")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the employee's email.",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log('Please enter an email!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Please enter the employee's github username.",
+            when: (input) => input.role === "Engineer",
+            validate: nameInput => {
+                if (nameInput ) {
+                    return true;
+                } else {
+                    console.log ("Please enter the employee's github username!")
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Please enter the intern's school",
+            when: (input) => input.role === "Intern",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true; 
+                } else {
+                    console.log ("Please enter the intern's school!")
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Would you like to add more team members?',
+            default: false 
+        }
+        ])
+    }
