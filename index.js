932d6eae035c6d61902ac21c3b3ddbfb85bc77a6
Tyler Shifflett <tylershifflett@Tylers-MacBook-Pro.local>
2022-05-07 15:15:16 -0400

    Add Const Profiles & Modules

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 const generateHTML = require('./src/generateHTML');
 
 // The different team profiles 
-const Manager 
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// Node Modules Enlisted 
+const fs = require('fs');
+const inquirer = require('inquirer');
